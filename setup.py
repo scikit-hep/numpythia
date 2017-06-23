32bf3ebea1f69acff5acd116b8ca2373b9571398
Noel Dawe <noel.dawe@gmail.com>
2017-06-23 00:53:18 -0300

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ else:
 
 setup(
     name='numpythia',
-    version='0.1.0',
+    version='0.2.0',
     description='The interface between PYTHIA and NumPy',
     long_description=''.join(open('README.rst').readlines()),
     maintainer='Noel Dawe',
