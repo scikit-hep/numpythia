f7e1ca87e44056dc61c70607c5673d4eef0e3d84
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2023-01-05 14:06:53 +0100

    Add support for Python 3.9 (#71)

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: C++',
         'Programming Language :: Cython',
         'Development Status :: 5 - Production/Stable',
